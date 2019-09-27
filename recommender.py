a574edc3c892ee60221f4d3843b0549ae8bcbab5
mozza5 <mozzarella1103@gmail.com>
2019-09-27 23:50:05 +0900

    0927_23_50

--- recommender.py
+++ recommender.py
@@ -73,3 +73,59 @@ area_work_pop.columns=['year','quater','area_level','area_level_name','area_code
 print(area_work_pop)
 
 print(area_work_pop[area_work_pop.area_code==num])
+
+target = area_work_pop[area_work_pop.area_code==num]
+print(target)
+area_work_pop[area_work_pop.area_code==num]
+
+# .iloc[행번호] {결과 데이터에서 차례대로 }
+#  .loc[인덱스번호]
+target= target.iloc[0]
+print(target)
+
+print(target.age_60_pop)
+print(target[0])
+
+# 출력 print('포맷 {0}  {1}'.format( , ....))
+# 반올림 round( {값}, {표현할 자리수})
+print('여성: {0}'.format(round(target.fml_worker_pop/target.total_worker_pop*100,1)))
+print('남성:',round(target.ml_worker_pop/target.total_worker_pop*100,1))
+
+print('여성')
+print('10대 {0}'.format(round(target.fmage_10_pop/target.fml_worker_pop*100,1)))
+print('20대 {0}'.format(round(target.fmage_20_pop/target.fml_worker_pop*100,1)))
+print('30대 {0}'.format(round(target.fmage_30_pop/target.fml_worker_pop*100,1)))
+print('40대 {0}'.format(round(target.fmage_40_pop/target.fml_worker_pop*100,1)))
+print('50대 {0}'.format(round(target.fmage_50_pop/target.fml_worker_pop*100,1)))
+print('60대 이상{0}'.format(round(target.fmage_60_pop/target.fml_worker_pop*100,1)))
+
+temp = 100/target.ml_worker_pop
+
+print('남성')
+for i in range(1,7):
+    print('{0}대 {1}'.format(i*10,round(target[14+i]*temp,1)))
+
+#### 소상공인협회에서 제공하는 상가 정보로 가게 데이터 입력
+## 서울시에서 제공하는 카테고리로 분류 할 것이기 때문에
+## 카테고리 매핑을 해야함
+
+## xrld  패키지 설치되어있어야함
+
+mapping = pd.read_excel('mapping.xlsx',skiprows=[0])
+
+# fillna NaN 값을 다른 값으로 대치
+# method = 'ffill''  : 앞의 값으로 nan 대치   vs  'bfil'  : 뒤의 값으로 nan 대치
+mapping = mapping.fillna(method='ffill')
+print(mapping)
+
+# 필요 없는 column 삭제
+mapping = mapping.drop(['세세분류코드','세세분류코드명','세분류코드명','소분류코드명','중분류코드명',
+                       '대분류코드명','대분류코드','대분류코드명.1','중분류코드','중분류코드명.1'],1)
+mapping.columns= ['대분류', '중분류', '소분류코드', '소분류']
+print(mapping)
+
+stores = pd.read_csv('stores.csv')
+
+stores = stores[stores['시도코드'] ==11]
+stores = stores[0:1000]
+print(stores)
---
+++ storeMapping.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+mapping =pd.read_excel('mapping.xlsx')
+print(mapping)
